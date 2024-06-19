84d75e6907a9b38e9e78c88a6f80fd62fca9711c
Emmanuel <onyejemeemmanuel65@gmail.com>
2024-06-19 10:30:16 +0100

    First commit

---
+++ src/test/java/com/ecobank/idea/IdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecobank.idea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
